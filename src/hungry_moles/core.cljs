b4b1780a66ddce3e8f8f351e6a10fc67d3b03bba
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-08-21 22:37:09 +0200

    Protocols and other changes

--- src/hungry_moles/core.cljs
+++ src/hungry_moles/core.cljs
@@ -2,77 +2,88 @@
     (:require [hungry-moles.protos :as p])
     (:require-macros [hungry-moles.macros :as m]))
 
-(def entities (atom
-               [;; {:key "bullet"
-                ;;  :asset {:src "assets/img/bullet.png"}
-                ;;  :body :arcade
-                ;;  :group 30}
-                ;; {:key "invader"
-                ;;  :asset {:src "assets/img/invader32x32x4.png"
-                ;;          :type :spritesheet
-                ;;          :x 32 :y 32}
-                ;;  :body :arcade
-                ;;  :group 30
-                ;;  }
-                ;; {:key "enemyBullet"
-                ;;  :asset {:src "assets/img/enemy-bullet.png"}
-                ;;  :group 30}
-                {:key "ship"
-                 :asset {:src "assets/img/player.png"}
-                 :x 300 :y 300
-                 }
-                ;; {:key "explode"
-                ;;  :asset {:src "assets/img/explode.png"}
-                ;;  };
-                
-                ;; {:key "background"
-                ;;  :asset {:src "assets/img/background.png"}
-                ;;  :x 200 :y 500}
-                ]))
+(defonce game (atom))
+(defonce entities (atom))
+(def entities-map [ {:key "ship"
+                     :asset {:src "assets/img/player.png"}
+                     :x 200
+                     :y 200} ])
 
-;; (def non-entities
-;;   [{:key "starfield"
-;;     :asset {:src "assets/img/starfield.png"}
-;;     :x 0 :y 0 :x2 800 :y2 600}])
-
-(defonce phaser-game (atom))
+(declare update-entity)
 
 (defn get-screen []
   (p/defscreen
     :size [800 600] 
     :title "invaders"
     :start-system :arcade
-    :states {:Play {:entities @entities
-                    :preload (fn [game] nil)
+    :states {:Play {:preload (fn [game]
+                               (doseq [e entities-map]
+                                 (m/call-in* game [-load image] (:key e) (:src (:asset e)))))
+                    
                     :create (fn [game]
-                              ;; (let [ship (m/call-in* @phaser-game [-world getChildAt] 0)]
-                              ;;   ;; (set! (m/call-in* ship [-x]) 300)
-                              ;;   ;; (set! (m/call-in* ship [-y]) 300)
-                              ;;   )
-                              )
-                    
+                              (let [e (map #(p/defentity game % #'p/physical) entities-map)
+                                    w (m/call-in* game [-world])]
+                                (reset! entities e)
+                                (p/add-entities w e)))
                     :update (fn [game]
-
-                              (let [upd-ents (map #(assoc %
-                                                          :x (+ 1 (:x %))
-                                                          :y (+ 1 (:y %))))]
-                                (update-entities (.-world game) ent))
-
-                              )
-                    
+                              (let [w (m/call-in* game [-world])
+                                    new (map update-entity @entities)]
+                                (reset! entities new)
+                                (p/update-entities w new)))
                     :render (fn [game] nil)}}))
 
-
-
+(defn update-entity [e]
+  (let [x (:x e)
+        y (:y e)
+        ue (-> e
+               (assoc :x (rand-int 600))
+               (assoc :y (rand-int 800)))]
+    ue))
 
 (defn start-game []
-  (let [game (get-screen)]
-    (reset! phaser-game game)
-    (.start (.-state game) "Play")))
+  (let [new-game (get-screen)]
+    (reset! game new-game)
+    (.start (.-state new-game) "Play")))
 
 (defn destroy-world []
-  (.destroy @phaser-game))
+  (.destroy @game))
 
 (defn restart-game []
   (destroy-world)
   (start-game))
+
+
+;; (def entities (atom
+;;                [;; {:key "bullet"
+;;                 ;;  :asset {:src "assets/img/bullet.png"}
+;;                 ;;  :body :arcade
+;;                 ;;  :group 30}
+;;                 ;; {:key "invader"
+;;                 ;;  :asset {:src "assets/img/invader32x32x4.png"
+;;                 ;;          :type :spritesheet
+;;                 ;;          :x 32 :y 32}
+;;                 ;;  :body :arcade
+;;                 ;;  :group 30
+;;                 ;;  }
+;;                 ;; {:key "enemyBullet"
+;;                 ;;  :asset {:src "assets/img/enemy-bullet.png"}
+;;                 ;;  :group 30}
+;;                 {:key "ship"
+;;                  :asset {:src "assets/img/player.png"}
+;;                  :x 300 :y 300
+;;                  }
+;;                 ;; {:key "explode"
+;;                 ;;  :asset {:src "assets/img/explode.png"}
+;;                 ;;  };
+                
+;;                 ;; {:key "background"
+;;                 ;;  :asset {:src "assets/img/background.png"}
+;;                 ;;  :x 200 :y 500}
+;;                 ]))
+
+
+;; (def non-entities
+;;   [{:key "starfield"
+;;     :asset {:src "assets/img/starfield.png"}
+;;     :x 0 :y 0 :x2 800 :y2 600}])
+
--- src/hungry_moles/protos.cljs
+++ src/hungry_moles/protos.cljs
@@ -10,48 +10,47 @@
   (update-entities [this entities]))
 
 (defprotocol IVisibleEntity
-  (add! [_ game])
-  (preload! [_ game]))
+  (pos-update! [this params])
+  (add! [this])
+  (create-primitive [this])
+  (preload [this]))
 
+(defprotocol IBodyEntity
+  (enable-body! [this]))
 
+(defrecord SpriteEntity [game x y key]
+  IVisibleEntity
+  (pos-update! [this params]
+    (let [p (:primitive this)]
+      (set! (.-x p) (:x params))
+      (set! (.-y p) (:y params))))
+  
+  (create-primitive [this]
+    (let [p (js/Phaser.Sprite. game (:x this) (:y this) (:key this))]
+      p))
+  
+  IBodyEntity
+  (enable-body! [this]
+    (let [p (:primitive this)]
+      (set! (.-physicsBodyType p) (.-ARCADE js/Phaser.Physics)))))
 
 ;; TYPES
-
-(deftype StaticEntity [entity]
-  IVisibleEntity
-  (add! [_ game]
-    (let [sprite (.sprite (.-add game) (:x entity) (:y entity) (:key entity))]
-      (.log js/console sprite)
-      sprite))
-  (preload! [_ game]
-    (.image (.-load game) (:key entity) (:src (:asset entity)))))
-
 
 (extend-type js/Phaser.World
   IManageEntities
   (add-entities [this entities]
-    (let [game (.-game this)
-          mapped-entities (map #(assoc % :sprite (add! (StaticEntity. %) game)) entities)]
-      (doseq [entity mapped-entities]
-        (if (:body entity)
-          (case (:body entity)
-            :arcade (set! (.-physicsBodyType (:sprite entity)) (.-ARCADE js/Phaser.Physics))
-            :else nil))
-        (.add this (:sprite entity)))
-      (set! (m/call-in* this [-entities] ) mapped-entities)))
+    (doseq [e entities]
+      (.log js/console e)
+      (doseq [s (:systems e)]
+        (let [p (:primitive s)]
+          (m/call-in* this [add] p)))))
   
-  (get-entities [this]
-    (m/call-in* this [-entities] ))
+  (get-entities [this] )
   
   (update-entities [this entities]
-    (let [stored-entities (get-entities this)]
-      (.log js/console stored-entities)
-      (doseq [ent entities]
-        (let [phy-ent (get stored-entities (:key ent))]
-          (set! (.-x phy-ent) (:x ent))
-          (set! (.-y phy-ent) (:y ent))))
-      ))
-  )
+    (doseq [e entities]
+      (doseq [s (:systems e)]
+        (pos-update! s e)))))
   
 
 (defn defscreen [& params]
@@ -60,24 +59,16 @@
     (when (:states params)
       (let [game (js/Phaser.Game. x y (.-auto js/Phaser)  (:title params))]
         (doseq [[state-name state-obj] (:states params)]
-          
           (.add (.-state game) (str (name state-name))
                 (fn [game]
                   (reify Object
                     ;; preload
                     (preload [_]
-                      (doseq [ent (or (:entities state-obj) {})]
-                        
-                        (m/call-in* game [-load image] (:key ent) (:src (:asset ent))))
-                      
-                      (:preload state-obj) game)
+                      ((:preload state-obj) game))
                     ;; create
                     (create [_]
                       (let [world (.-world game)]
-                        (set! (.-enableBody world) true)
-                        
-                        (add-entities world (:entities state-obj)))
-                      
+                        (set! (.-enableBody world) true))
                       (if-let [sys (:start-system params)]
                         (cond
                           (= :arcade sys) (.startSystem (.-physics game) (.-ARCADE js/Phaser.Physics))
@@ -87,5 +78,20 @@
                     (update [_] ((:update state-obj) game))
                     ;; render
                     (render [_] ((:render state-obj) game))))))
-        game)))
-  )
+        game))))
+
+
+;; systems
+(defn physical [game params]
+  (let [s (SpriteEntity. game (:x params) (:y params) (:key params)) 
+        v (create-primitive s)
+        sys-enabled (assoc s :primitive v)]
+    (enable-body! sys-enabled)
+    sys-enabled))
+
+;; helpers
+(defn defentity [game params & systems]
+  (assoc params :systems (map #(% game params) systems)))
+
+(defn context [f with]
+  (partial f with))
